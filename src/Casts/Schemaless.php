d8e8ec2914a54785a5bfd81be5427b2eb5b3764a
Paulo Scheuermann <paulo@rockbuzz.com.br>
2021-08-16 11:18:38 -0400

    feat: adicionado possibilidade de usar casts sem uma classe base.

--- src/Casts/Schemaless.php
+++ src/Casts/Schemaless.php
@@ -5,33 +5,34 @@ namespace Rockbuzz\LaraUtils\Casts;
 use Throwable;
 use DomainException;
 use Illuminate\Support\Collection;
+use Rockbuzz\LaraUtils\Schemaless\Base;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class Schemaless implements CastsAttributes
 {
-    private string $class;
+    private ?string $class;
 
-    public function __construct(string $class)
+    public function __construct(string $class = null)
     {
         $this->class = $class;
     }
 
-    /** @inheritDoc
+    /**
+     * @inheritDoc
      * @throws Throwable
      */
     public function get($model, string $key, $value, array $attributes)
     {
-        $settings = isset($attributes[$key]) ? json_decode($attributes[$key], true) : [];
-
         throw_if(
-            !class_exists($this->class),
+            $this->class && !class_exists($this->class),
             new DomainException("The {$this->class} not found")
         );
 
-        return new $this->class($settings);
+        return $this->getCastClass(isset($attributes[$key]) ? json_decode($attributes[$key], true) : []);
     }
 
-    /** @inheritDoc
+    /**
+     * @inheritDoc
      * @throws Throwable
      */
     public function set($model, string $key, $value, array $attributes): array
@@ -42,5 +43,19 @@ class Schemaless implements CastsAttributes
         );
 
         return [$key => json_encode($value instanceof $this->class ? $value : $model->$key->merge($value))];
+    }
+
+    private function getCastClass($value): Base
+    {
+        if ($this->class) {
+            return new $this->class($value);
+        }
+
+        return new class extends Base {
+            protected function getBaseItems(): Collection
+            {
+                return collect();
+            }
+        };
     }
 }

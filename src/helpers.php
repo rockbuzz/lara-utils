7ae375621cfa351c6c451dba66448e8b4f153784
Paulo Scheuermann <paulo@rockbuzz.com.br>
2021-11-08 10:37:04 -0400

    chore: adicionado funções de helper para ajudar no calculo de valores monetários

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,24 @@ if (!function_exists('logging')) {
         return new Logging($logable, $context);
     }
 }
+
+if (!function_exists('percentage_of')) {
+    function percentage_of(int $percentage, float $float): float
+    {
+        return $float * ($percentage / 100);
+    }
+}
+
+if (!function_exists('format_currency')) {
+    function format_currency(int $cents): float
+    {
+        return (float)number_format($cents / 100, 2, '.', '');
+    }
+}
+
+if (!function_exists('to_pennies')) {
+    function to_pennies(float $float): int
+    {
+        return (int)$float * 100;
+    }
+}
